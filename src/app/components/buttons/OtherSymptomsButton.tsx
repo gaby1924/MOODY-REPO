eb94ae5fc9bae260170ee2c2cd9860f108dbdcc4
Gaby Kaul <gkaul13@gmail.com>
2026-08-09 23:27:58 -0400

    commit push to moody-repo

---
+++ src/app/components/buttons/OtherSymptomsButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useState } from "react";
+
+export default function OtherSymptomsButton() {
+    const [otherSymptoms, setOtherSymptoms] = useState<boolean | null>(null);
+    const [text, setText] = useState("");
+
+    return (
+        <div className="card">
+             <h3>Other symptoms?</h3>
+
+      <div className="button-row">
+        <button onClick={() => setOtherSymptoms(true)}>Yes</button>
+        <button onClick={() => setOtherSymptoms(false)}>No</button>
+      </div>
+
+    {
+        otherSymptoms && (
+            <textarea
+                placeholder="Describe other symptoms..."
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                className="textarea"
+            />
+        )
+    }
+    </div >
+  );
+}
